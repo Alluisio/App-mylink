32edcf31ea1f065eaf9344b7b918e2e459c1a5ba
Alluisio Silva <alluisio.silva@ipdec.org>
2021-06-01 23:55:08 -0400

    Customização e implementação da StatusBar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text } from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+import StatusBarPage from "../../components/StatusBarPage";
 
 const Home = () => {
   return (
-    <View>
+    <LinearGradient
+      colors={["#1dddd9", "#132742"]}
+      style={{ flex: 1, justifyContent: "center" }}
+    >
+      <StatusBarPage backgroundColor="#1dddd9" barStyle="light-content" />
       <Text>Página Home</Text>
-    </View>
+    </LinearGradient>
   );
 };
 
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import StatusBarPage from "../../components/StatusBarPage";
+
 const MyLinks = () => {
   return (
     <View>
+      <StatusBarPage backgroundColor="#132742" barStyle="light-content" />
       <Text>Página Links</Text>
     </View>
   );
