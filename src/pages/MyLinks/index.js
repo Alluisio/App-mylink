4aac5715e82efa16108eaf6c98d0268dc63d3d7d
Alluisio Silva <alluisio.silva@ipdec.org>
2021-06-01 22:21:28 -0400

    Criação da tela MyLinks

---
+++ src/pages/MyLinks/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const MyLinks = () => {
+  return (
+    <View>
+      <Text>Página Links</Text>
+    </View>
+  );
+};
+
+export default MyLinks;
