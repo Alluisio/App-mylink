4bd396f87ac814b777b1e5fbf941c1e3b8061605
Alluisio Silva <alluisio.silva@ipdec.org>
2021-06-01 22:21:13 -0400

    Criação da tela Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Página Home</Text>
+    </View>
+  );
+};
+
+export default Home;
