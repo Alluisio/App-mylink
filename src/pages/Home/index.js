5f808a6582a3f516548239453a74dd31016e893b
Alluisio Silva <alluisio.silva@ipdec.org>
2021-06-04 00:12:33 -0400

    Modificações de usabilidade para o teclado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
 import StatusBarPage from "../../components/StatusBarPage";
@@ -21,35 +27,48 @@ import {
 
 const Home = () => {
   return (
-    <LinearGradient
-      colors={["#1dddd9", "#132742"]}
-      style={{ flex: 1, justifyContent: "center" }}
-    >
-      <StatusBarPage backgroundColor="#1dddd9" barStyle="light-content" />
-      <Menu />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <LinearGradient
+        colors={["#1dddd9", "#132742"]}
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <StatusBarPage backgroundColor="#1dddd9" barStyle="light-content" />
+        <Menu />
 
-      <ContainerLogo>
-        <Logo source={require("../../assets/Logo.png")} resizeMode="contain" />
-      </ContainerLogo>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "android" ? "padding" : "position"}
+          enabled
+        >
+          <ContainerLogo>
+            <Logo
+              source={require("../../assets/Logo.png")}
+              resizeMode="contain"
+            />
+          </ContainerLogo>
 
-      <ContainerContent>
-        <Title>SujeitoLink</Title>
-        <SubTitle>Cole seu link para encurtar</SubTitle>
-        <ContainerInput>
-          <BoxIcon>
-            <Feather name="link" size={22} color="#fff" />
-          </BoxIcon>
-          <Input
-            placeholder="Cole seu link aqui..."
-            placeholderTextColor="white"
-          />
-        </ContainerInput>
+          <ContainerContent>
+            <Title>SujeitoLink</Title>
+            <SubTitle>Cole seu link para encurtar</SubTitle>
+            <ContainerInput>
+              <BoxIcon>
+                <Feather name="link" size={22} color="#fff" />
+              </BoxIcon>
+              <Input
+                placeholder="Cole seu link aqui..."
+                placeholderTextColor="white"
+                autoCaptalize="none"
+                autoCorrect={false}
+                keyboardType="url"
+              />
+            </ContainerInput>
 
-        <ButtonLink>
-          <ButtonLiknkText>Gerar Link</ButtonLiknkText>
-        </ButtonLink>
-      </ContainerContent>
-    </LinearGradient>
+            <ButtonLink>
+              <ButtonLiknkText>Gerar Link</ButtonLiknkText>
+            </ButtonLink>
+          </ContainerContent>
+        </KeyboardAvoidingView>
+      </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 };
 
